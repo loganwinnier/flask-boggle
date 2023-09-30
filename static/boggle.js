ee3fbee0648bf3f2554c25c49890a51258090b4a
Logan Winnie <loganwinnier@gmail.com>
2023-09-29 20:14:16 -0400

    finsihed have some stuff to fit

--- static/boggle.js
+++ static/boggle.js
@@ -27,15 +27,15 @@ async function start() {
 
 function displayBoard(board) {
   $table.empty();
-  // loop over board and create the DOM tr/td structure
-  // make main part of board
+
   const $tbody = $('<tbody>');
 
   for (let y = 0; y < board.length; y++) {
     const $row = $('<tr>');
 
     for (let x = 0; x < board.length; x++) {
-      const $cell = $(`<td>${board[y][x]}</td>`).attr('id', `c-${y}-${x}`).data("letter", board[y][x]);
+      const $cell = $(`<td>${board[y][x]}</td>`);
+
       $row.append($cell);
     }
     $tbody.append($row);
@@ -43,36 +43,43 @@ function displayBoard(board) {
   $table.append($tbody);
 }
 
-async function handleGoClick(evt){
+/** Handle click of go button. gets value from word Input field and responds
+ * with error message or adds to played words*/
+
+async function handleGoClick(evt) {
+  //Controller Function
   evt.preventDefault();
 
   $message.empty();
 
-  const $wordInput = $("#wordInput");
-  const word = $wordInput.value();
+  const word = $wordInput.val().toUpperCase();
 
-  const response = await fetch("/api/score-word",{
-      method: "POST",
-      body: JSON.stringify({ gameId, word }),
-      headers: {
-        "content-type": "application/json",
-      }
+  //Seperate Function
+  const response = await fetch("/api/score-word", {
+    method: "POST",
+    body: JSON.stringify({ gameId, word }),
+    headers: {
+      "content-type": "application/json",
+    }
   });
 
   const { result } = await response.json();
 
-  console.log("result: ", result)
+  //Determine where should go function
+  if (result === "ok") {
 
-  if(result === "ok"){
-    console.log("Ok triggered")
-    const $newLi = $("<li>").text(word)
+    const $newLi = $("<li>").text(word);
     $playedWords.append($newLi);
+
   } else {
-    $message.text(`${result}`)
+    $message.text(`${word} is ${result}`);
   }
 
+  //DOM Manipulation
+
+  $form[0].reset();
 }
 
-$("#word-input-btn").on("click", handleGoClick);
+$form.on("submit", handleGoClick);
 
 start();
